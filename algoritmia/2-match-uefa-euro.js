c3da8cad8a7557fd1658ccda0889e5b4f9201c49
Gi029 <kj.gadea@gmail.com>
2025-04-17 16:12:19 +0200

    faltan 2 ejercicos

--- algoritmia/2-match-uefa-euro.js
+++ algoritmia/2-match-uefa-euro.js
@@ -8,9 +8,25 @@
  * @returns {string} Resultado del partido, siguiendo el formato indicado en los comentarios, según si ha ganado el primer equipo, el segundo, o ha habido empate.   
 */
 function uefaEuro2016(teams, scores) {
+     let ganador = " ";
 
+if (scores[0] > scores[1]){
+    ganador = teams[0];
+    return "At match " + teams[0] + " - " + teams[1] +  ", " + ganador + " won!";
+
+
+}else if( scores[1] > scores [0]){
+    ganador = teams [1];
+    return "At match " + teams[1] + " - " + teams[0] +  ", " + ganador + " won!";
+    
+}else {
+    //esto es empate
+    return "At match " + teams[0] + " - " + teams[1] + ",teams played draw.";
+}
+    
 }
 
+                        //   [0]        [1]      [0] [1]
 console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0])) // "At match Germany - Ukraine, Germany won!"
 console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2])) // "At match Belgium - Italy, Italy won!"
 console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1])) // "At match Portugal - Iceland, teams played draw."
